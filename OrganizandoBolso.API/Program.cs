2f1da11df3dea270f78d057d43104e29e051189e
Thales Lima <thaleslimadejesus@gmail.com>
2025-10-11 21:13:30 -0300

    fix: enable swagger in production

--- OrganizandoBolso.API/Program.cs
+++ OrganizandoBolso.API/Program.cs
@@ -105,15 +105,12 @@ builder.Services.AddHostedService<OrganizandoBolso.Repository.HostedServices.Mon
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "OrganizandoBolso API v1");
-        c.RoutePrefix = string.Empty;
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "OrganizandoBolso API v1");
+    c.RoutePrefix = "api/docs";
+});
 
 app.UseHttpsRedirection();
 
